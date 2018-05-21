4967c6d1d73a0ffc43ca95f4f94261d749dc2d55
AntonioMota0014 <32169072+AntonioMota0014@users.noreply.github.com>
2018-05-21 10:47:36 +0100

    Update Pilha.java

--- Pilha.java
+++ Pilha.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pilha;
 
-/**
- *
- * @author alunosepi
- */
 public class Pilha {
 
  public Object[] pilha;
